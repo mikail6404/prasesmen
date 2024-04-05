3b58e5c08647dd3ed479f8faa6d3234a59119e84
Muhammad Mikail Gabril <114597845+mikail6404@users.noreply.github.com>
2024-04-05 15:46:56 +0700

    v.1.1 Javascript function to search array and validate value

--- asesmen/form.php
+++ asesmen/form.php
@@ -16,7 +16,8 @@ c. 1 tombol
     <img src="mikail.jpg" width="200" height="200">
     <h2>NAMA: M.MIKAIL GABRIL <br> 6701223002 <br> 46-02 </h2>
     <h3><b>Cari Data Buku</b></h3>
-    <form action="form.php" method="POST">
+    <form action="form.php" method="POST" onsubmit="return validateForm()">
+       
         <tr>
             <td>Jumlah Buku</td>
             <td>:</td>
@@ -24,20 +25,49 @@ c. 1 tombol
             <br />
         </tr>
         <tr>
-            <input type="radio" id="sherlock" name="buku" value="Sherlock">
-              <label for="html">Sherlock</label><br>
-              <input type="radio" id="lupin" name="buku" value="Lupin">
+            <input type="radio" id="buku" name="buku" value="sherlock">
+              <label for="sherlock">Sherlock</label><br>
+              <input type="radio" id="buku" name="buku" value="lupin">
               <label for="lupin">Lupin</label><br>
-              <input type="radio" id="aladdin" name="buku" value="Aladdin">
+              <input type="radio" id="buku" name="buku" value="aladdin">
               <label for="aladdin">Aladdin</label>
             <br />
         </tr>
         <td>
+//Jika jumlah tidak kosong, lakukan pencarian array di atas. Jika pilihan buku tidak ditemukan, tampilkan alert bahwa buku tidak ditemukan
+//Jika buku yang dipilih ditemukan dalam array, cek apakah jumlah yang diinputkan lebih banyak dari jumlah buku dalam array. Jika jumlah dari textfield lebih banyak dari array, tampilkan alert bahwa jumlah buku tidak tersedia.
+//Jika Jumlah yang diinputkan sama atau kurang dari stock jumlah yang ada di array, tampilkan “silahkan ambil buku anda”
+
             <input type="submit" name="search" value="search" />
         </td>
     </form>
 
+<script>
+    let buku = [{
+            "nama": "sherlock",
+            "jumlah": 2
+        },
+        {
+            "nama": "lupin",
+            "jumlah": 2
+        },
+        {
+            "nama": "aladdin",
+            "jumlah": 2
+        },
+    ]
+     //Buatlah fungsi dengan javascript untuk mengambil jumlah dan select option yang dipilih
+    function validateForm() {
+            let jumlah = document.getElementById('jumlah').value;
+            let selectedBook = document.querySelector('input[name="buku"]:checked').value;
 
+            if (jumlah.trim() === '') {
+                alert('Jumlah buku tidak boleh kosong.');
+                return false;
+            }
+        }
+    
+</script>
 
 </body>
 
